b1e0cf240235265574b7378a7c26be4e23a55969
Ricardo Corral C <ricardocorralc@gmail.com>
2017-02-01 21:39:10 -0600

    running version

--- cafa3_standalone.py
+++ cafa3_standalone.py
@@ -3,9 +3,13 @@ import config
 from itertools import groupby
 import requests
 import json
+import logging
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 targets_dir = config.paths['targets_dir']
 endpoint = config.paths['predictor_endpoint']
+file_header_str = config.file_header_str
 
 headers = {"Content-Type": "application/json"}
 
@@ -28,28 +32,39 @@ def fasta_iter(fasta_name):
         seq = "".join(s.strip() for s in faiter.next())
         yield header, seq
 
+def sort_list(scores_list=None):
+    scores_list = sorted(scores_list, key=lambda tup: tup[1], reverse=True)
+    return scores_list
+
 def to_list_from_dict_format(scores_dict=None, score_key='score'):
     (_k, ) = set(scores_dict[0].keys()) - set([score_key])
     return sort_list([(_[_k], _[score_key]) for _ in scores_dict])
 
 def to_cafa_scorlist_format(scores_list=None, sample_name="T00203", n=5):
-    return "\n".join(["%s\t%s\t%s" % (sample_name, _n, _s)
+    return "\n".join(["%s\t%s\t%.7f" % (sample_name, _n, _s)
                       for _n, _s in scores_list[:n]])
         
 def main():
     ontologies = ['C','F']
-    n = 2
+    n = 1500/len(ontologies)
     for filename, path in (iter_directory_files()):
         print path
         taxonID = filename[len('target.'):-(len('.fasta'))]
         print 'taxonID', taxonID
-        for target_name, seq in fasta_iter(path):
-            _tn = target_name.split()[0]
-            data = {'seq':seq}
-            for onto in ontologies:
-            r = requests.post(endpoint+'/'+onto+'/?n='+str(n), data=json.dumps(data), headers=headers)
-            print json.dumps(r)
-            exit()
+        with open('Corraleads_1_'+taxonID+'.txt','w') as fout:
+            fout.write("%s" % file_header_str)
+            for target_name, seq in fasta_iter(path):
+                _tn = target_name.split()[0]
+                logging.info("\n\nProcessing: %s" % _tn)
+                data = {'seq':seq}
+                for onto in ontologies:
+                    _ep = endpoint+onto+'/?n='+str(n)
+                    r = requests.post(_ep, data=json.dumps(data), headers=headers).json()
+                    scores_dict = r["predictions"][0]["prediction"]
+                    scores_list = to_list_from_dict_format(scores_dict)
+                    cafa_out = to_cafa_scorlist_format(scores_list=scores_list,sample_name=_tn,n=n)
+                    fout.write("%s\n" % cafa_out )
+            fout.write("%s" % "END")
 
 
 if __name__ == '__main__':
