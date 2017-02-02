bce017ade5d21b52ad229a5a73b4339b0aca92db
Ricardo Corral Corral <ricardo@suggestic.com>
2017-02-01 21:20:18 -0600

    add file_header_str string

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
 paths = {'targets_dir':'CAFA3_targets/Target files',
          'predictor_endpoint':'http://172.17.0.2:5000/predict/'}
+
+file_header_str = """
+AUTHOR corraleads
+MODEL 1
+KEYWORDS deep_learning, m-lom
+"""
